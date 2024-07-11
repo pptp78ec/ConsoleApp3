e25bdb0267d7f243d7b1e4b4a0270b6493cd57bd
pptp78ec <48621910+pptp78ec@users.noreply.github.com>
2024-07-11 13:21:04 +0300

    маленькие улучшения

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -7,7 +7,7 @@ namespace ConsoleApp3
         static void Main(string[] args)
         {
              var newcalc = new NewCalcMethod(){ AgeOfFertility = 100, Lifespan = 2000, Startpopulation = 1000, TFR = 2.1, Timespan = 10000 };
-             System.Console.WriteLine( $"Total pop after {newcalc.Timespan} years is: {String.Format("{0:N}", newcalc.DoCalc())}");
+             System.Console.WriteLine( $"Parameters:\nFertility age: {newcalc.AgeOfFertility},\nLifespan: {newcalc.Lifespan},\nStarting pop.: {newcalc.Startpopulation},\nTotal Fertility Rate: {newcalc.TFR}.\n*******\nTotal pop after {newcalc.Timespan} years is: {String.Format("{0:N}", newcalc.DoCalc())}");
         }
     }
 }
